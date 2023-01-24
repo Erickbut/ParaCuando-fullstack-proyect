06f0f7fe288644ba75dd025db9d9285c7a87d2d7
AleAlejandro0 <alealejandropaez@gmail.com>
2023-01-23 20:59:59 -0500

    feat: static and dynamic components were added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 
+//Atom imports
+import BrandsAndStylesAtom from '../components/BrandsAndStylesAtom'
+import ArrowAtom from '../components/ArrowAtom'
+import LikesAtom from '../components/LikesAtom'
+import NextButtonsAtom from '../components/NextButtonsAtom'
+import SearchAtom from '../components/SearchAtom'
+
 //Import Layout header and footer 
 import Layout from '../components/Layout'
 
@@ -12,6 +19,11 @@ export default function Home() {
   return (
     <Layout>
        <h1>hello world</h1>
+       <BrandsAndStylesAtom/>
+       <ArrowAtom/>
+       <LikesAtom/>
+       <NextButtonsAtom/>
+       <SearchAtom/>
      </Layout>
   )
 }
